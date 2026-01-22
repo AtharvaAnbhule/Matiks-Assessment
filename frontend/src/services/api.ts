be0a321947257029b1878a2d3a4152006c5d44e7
Atharva Anbhule <atharvaanbhule@gmail.com>
2026-01-22 12:02:02 +0530

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 import Constants from 'expo-constants';
 
 
-const API_BASE_URL = Constants?.expoConfig?.extra?.apiBaseUrl || 'https://matiks-assessment.onrender.com';
+const API_BASE_URL = 'https://matiks-assessment.onrender.com';
 
 const axiosInstance: AxiosInstance = axios.create({
     baseURL: API_BASE_URL,
